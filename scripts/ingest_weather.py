8f0793517a0cef1b7711bc2d0b63ce45b325b4b6
ji-0x <jamininia.dev@gmail.com>
2026-05-28 19:12:51 +1200

    feat: added function to load config file

--- scripts/ingest_weather.py
+++ scripts/ingest_weather.py
@@ -27,8 +27,41 @@ LOG_DIR = os.path.join(BASE_DIR, 'logs')
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
-print(api_key)
+#print(api_key)
 
 if not api_key:
     sys.exit("ERROR: API_KEY not foiund in .env")
 
+
+# -----------------------------
+# Load API Key and Cities
+# -----------------------------
+
+def load_config(path):
+    try:
+        with open(path, 'r') as f:
+            config = json.load(f)
+            return config
+
+    except FileNotFoundError:
+        sys.exit(f"ERROR: Config file not found: {path}")
+
+    except json.JSONDecodeError:
+        sys.exit(f"ERROR: Invalid JSON in {path}")
+
+
+
+def main():
+
+    # Load confi.jsong
+    config = load_config(CONFIG_PATH)
+    # Get cities
+    cities = config.get('cities', {})
+
+    print("API_KEY:", api_key)
+    print("Cities:", cities)
+
+
+
+if __name__ == '__main__':
+    main()
